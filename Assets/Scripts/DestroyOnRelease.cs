a30d1e7486aae29755d79ddaa7062b499e0f7efa
zzahra2 <zzahra2@uic.edu>
2019-11-17 18:22:04 -0600

    Updates to sword
    > Sword now has silhouette outline when picked up
    > Sword can be grabbed without needing to hold the trigger
    > Sword can be returned to starting dock
    > Sword can no longer be thrown

---
+++ Assets/Scripts/DestroyOnRelease.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using System.Collections;
+
+namespace Valve.VR.InteractionSystem
+{
+    [RequireComponent(typeof(Interactable))]
+    public class DestroyOnRelease : MonoBehaviour
+    {
+        private void OnDetachedFromHand(Hand hand)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
