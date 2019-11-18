abd124f7655222b1c7fd698f1ad42639e0f44c77
Juan Moraza <j.moraza@yahoo.com>
2019-11-17 19:06:30 -0600

    merged

---
+++ Assets/Models/Block/spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sp : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Transform[] point;
+    public float beat;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > beat)
+        {
+            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], point[Random.Range(0, 4)]);
+            cube.transform.localPosition = Vector3.zero;
+            //cube.transform.Rotate()
+            timer -= beat;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    //public Transform bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * transform.forward * -2;
+
+        //Transform bullet = Instantiate(bulletPrefab) as Transform;
+        //Physics.IgnoreCollision(bullet.GetComponent<Collider>(), GetComponent<Collider>());
+    }
+}
