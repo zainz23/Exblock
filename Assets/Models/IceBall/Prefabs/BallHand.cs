e675e1fe3728b9c1d596b37e6ebd878ba889b229
zzahra2 <zzahra2@uic.edu>
2019-11-22 07:29:04 -0600

    Adds ice ball weapon + custom particles
    > Can be picked up and put back like previous weapons
    > Used Unity's particle system for 3 unique prefab models

---
+++ Assets/Models/IceBall/Prefabs/BallHand.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class BallHand : MonoBehaviour
+    {
+        public GameObject prefab;       // The thing we are poofing up
+        public Rigidbody attachPoint;   // Used for velocity calculations
+
+        public SteamVR_Action_Boolean spawn;
+
+        SteamVR_Behaviour_Pose trackedObj;
+        FixedJoint joint;
+
+        private void Awake()
+        {
+            trackedObj = GetComponent<SteamVR_Behaviour_Pose>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (joint == null && spawn.GetStateDown(trackedObj.inputSource))
+            {
+                GameObject go = GameObject.Instantiate(prefab);
+                go.transform.position = attachPoint.transform.position;
+
+                joint = go.AddComponent<FixedJoint>();
+                joint.connectedBody = attachPoint;
+            }
+            else if (joint != null && spawn.GetStateUp(trackedObj.inputSource))
+            {
+                GameObject go = joint.gameObject;
+                Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+                Object.DestroyImmediate(joint);
+                joint = null;
+                // Destroy after 15s
+                Object.Destroy(go, 15.0f);
+
+                Transform origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
+                if (origin != null)
+                {
+                    rigidbody.velocity = origin.TransformVector(trackedObj.GetVelocity());
+                    rigidbody.angularVelocity = origin.TransformVector(trackedObj.GetAngularVelocity());
+                }
+                else
+                {
+                    rigidbody.velocity = trackedObj.GetVelocity();
+                    rigidbody.angularVelocity = trackedObj.GetAngularVelocity();
+                }
+
+                rigidbody.maxAngularVelocity = rigidbody.angularVelocity.magnitude;
+            }
+        }
+
+    }
+}
