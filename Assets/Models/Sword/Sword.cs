ccf5d97e83eba0c0ed7bb024daf0a865384a250f
zzahra2 <zzahra2@uic.edu>
2019-11-27 16:09:05 -0600

    Adds weapon RuneHammer; Can be picked up like other weapons

--- Assets/Models/Sword/Sword.cs
+++ Assets/Models/Sword/Sword.cs
@@ -6,19 +6,6 @@ namespace Valve.VR.InteractionSystem
 {
     public class Sword : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        //-------------------------------------------------
         void OnCollisionEnter(Collision collision)
         {
 
