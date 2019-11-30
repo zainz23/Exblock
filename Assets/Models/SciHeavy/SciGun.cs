dab1adaf7927bb20df7efc8e2ab00ea87d55fc6c
zzahra2 <zzahra2@uic.edu>
2019-11-30 16:02:11 -0600

    Adds SciPistol and SciRifle;
    > Same interaction system as SciHeavy: mags can be taken out of gun and reloaded
    > Guns can be shot and their respective sounds are added
    > Mag for all guns are now properly registered
    > Pistol and Rifle have rest poses
    > Script refactors for reusability

--- Assets/Models/SciHeavy/SciGun.cs
+++ Assets/Models/SciHeavy/SciGun.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 namespace Valve.VR.InteractionSystem
 {
-    public class SciHeavy : MonoBehaviour
+    public class SciGun : MonoBehaviour
     {
         // a reference to the action
         public SteamVR_Action_Boolean fire;
@@ -18,17 +18,24 @@ namespace Valve.VR.InteractionSystem
 
         private GameObject spawnLocation;
 
-        static private bool firstTimeGrab = false;
 
         // Ammo to display over gun
         public Text ammoText;
-
 
         // Clicking sound when out of ammo
         public AudioClip gunShotSound;
         // Clicking sound when out of ammo
         public AudioClip clickSound;
         private AudioSource audioSource;
+
+        public enum GunType
+        {
+            SciHeavy,
+            SciPistol,
+            SciRifle,
+            SciSniper
+        }
+        public GunType prefabTag = GunType.SciHeavy;    // Default
 
         void Start()
         {
@@ -45,40 +52,66 @@ namespace Valve.VR.InteractionSystem
 
         void Update()
         {
-            ammoText.text = Ammo.ammoSciHeavy.ToString() + " / " + Ammo.maxAmmoSciHeavy.ToString();
+            switch (prefabTag.ToString() )
+            {
+                case "SciHeavy":
+                    ammoText.text = Ammo.ammoSciHeavy.ToString() + " / " + Ammo.maxAmmoSciHeavy.ToString();
+                    break;
+                case "SciPistol":
+                    ammoText.text = Ammo.ammoSciPistol.ToString() + " / " + Ammo.maxAmmoSciPistol.ToString();
+                    break;
+                case "SciRifle":
+                    ammoText.text = Ammo.ammoSciRifle.ToString() + " / " + Ammo.maxAmmoSciRifle.ToString();
+                    break;
+                case "SciSniper":
+                    ammoText.text = Ammo.ammoSciSniper.ToString() + " / " + Ammo.maxAmmoSciSniper.ToString();
+                    break;
+            }
+            
         }
 
         void Fire(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
         {
-            // Dont instantiate bullet if we're using trigger to grab gun
-            /*
-            if (!firstTimeGrab)
+            if (prefabTag.ToString() == "SciHeavy")
             {
-                firstTimeGrab = true;
-                return;
+                AmmoCheck(ref Ammo.ammoSciHeavy);
             }
-            */
-            if (Ammo.ammoSciHeavy > 0)
+            else if (prefabTag.ToString() == "SciPistol")
+            {
+                AmmoCheck(ref Ammo.ammoSciPistol);
+            }
+            else if (prefabTag.ToString() == "SciRifle")
+            {
+                AmmoCheck(ref Ammo.ammoSciRifle);
+            }
+            else if (prefabTag.ToString() == "SciSniper")
+            {
+                AmmoCheck(ref Ammo.ammoSciSniper);
+            }
+        }
+
+        // Changes the passed in ammo parameter
+        //      x should be the static variable from Ammo.cs
+        void AmmoCheck(ref int ammo)
+        {
+            if (ammo > 0)
             {
                 Rigidbody instantiatedProjectile = Instantiate(projectile, spawnLocation.transform.position, spawnLocation.transform.rotation) as Rigidbody;
                 instantiatedProjectile.velocity = spawnLocation.transform.TransformDirection(new Vector3(0, 0, speed));
-                Ammo.ammoSciHeavy -= 1;
+                ammo -= 1;
                 // Play gun shot
                 audioSource.PlayOneShot(gunShotSound, 0.25f);
             }
-            else if (Ammo.ammoSciHeavy == 0)
+            else if (ammo == 0)
             {
                 // out of ammo
                 audioSource.PlayOneShot(clickSound, 0.5f);
-
             }
-
         }
 
         void OnDestroy()
         {
             fire.RemoveOnStateDownListener(Fire, handType);
-            firstTimeGrab = false;  // Reset incase user grabs gun again
         }
     }
 }
---
+++ Assets/Models/SciPistol/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem
+{
+    public class Bullet : MonoBehaviour
+    {
+        public enum TagList
+        {
+            blueCube,
+            yellowCube,
+            redCube,
+            grayCube,
+            runeCube
+        }
+        // The tag we want to collide with
+        public TagList collideTag = TagList.redCube;
+
+        void OnCollisionEnter(Collision collision)
+        {
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            bool hitBlock = collision.collider.gameObject.GetComponent<Block>() != null;
+
+            if (hitBlock && collision.gameObject.tag.Equals(collideTag.ToString() ))
+            {
+                collision.collider.gameObject.SendMessageUpwards("ApplyDamage", SendMessageOptions.DontRequireReceiver);
+                gameObject.SendMessage("HasAppliedDamage", SendMessageOptions.DontRequireReceiver);
+            }
+        }
+
+    }
+}
