b2669abef3087e910147c77aef101b2808e1698e
Juan <j.moraza@yahoo.com>
2019-11-30 13:04:35 -0600

    healthbar added, message for second wave, score not 100% working

---
+++ Assets/scoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreText : MonoBehaviour
+{
+    // Start is called before the first frame update
+     void Start()
+    {
+ 
+    }
+    public void updateText()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+
+    }
+}
---
+++ Assets/secondWave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class secondWave : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public Transform[] point;
+
+
+     void Start()
+    {
+ 
+        GameObject weapon1 = Instantiate(weapons[0], point[0]);
+        GameObject weapon2 = Instantiate(weapons[1], point[1]);
+
+ 
+        weapon1.transform.localPosition = Vector3.zero;
+        weapon2.transform.localPosition = Vector3.zero;
+        gameObject.SetActive(false);
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
