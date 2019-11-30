e9e733681fef857faae8ca88fb3a151b9c792b8e
Juan <j.moraza@yahoo.com>
2019-11-30 17:21:41 -0600

    dynamic beat track, next waepons message, and break point for second wave

--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -11,24 +11,32 @@ public class spawner : MonoBehaviour
     public AudioSource audioData;
     private float timer;
     private Queue myStack = new Queue();
-    private float[] floatArray = { 6.06f, 7.08f, 8.09f, 9.09f, 10.11f, 11.12f, 12.12f, 12.13f, 14.14f, 14.15f, 15.16f, 16.17f, 17.18f, 18.18f, 18.19f, 19.19f, 20.20f, 20.21f, 21.21f, 22.22f, 22.23f, 23.24f, 23.24f, 24.25f, 25.25f, 26.26f, 37.37f, 38.38f, 38.39f, 39.39f, 39.40f, 40.41f, 41.42f, 42.42f, 42.43f, 43.43f, 44.44f, 44.45f, 45.45f, 46.46f, 47.48f, 48.49f, 49.49f, 50.50f, 50.50f, 51.51f, 52.52f, 52.53f, 53.53f, 54.54f, 54.55f, 55.56f, 55.56f, 56.57f, 57.57f, 58.59f, 59.59f, 60.01f, 61.01f, 62.02f, 62.03f, 63.04f, 64.05f, 65.05f, 66.06f, 66.07f, 67.08f, 68.08f, 69.09f, 70.10f, 70.11f, 71.12f, 71.12f, 72.13f, 73.13f, 74.14f, 74.14f, 75.15f, 76.16f, 76.17f, 77.17f, 78.18f, 78.19f, 79.19f, 79.20f, 80.21f, 81.22f, 82.22f, 82.22f, 83.24f, 84.24f, 84.25f, 85.25f, 86.26f, 86.27f, 87.28f, 87.28f, 88.29f, 89.30f, 90.30f, 95.36f, 96.37f, 97.38f, 98.38f, 98.38f, 99.39f, 100.40f, 101.41f, 102.42f, 102.43f, 103.44f, 103.44f, 104.45f, 105.46f, 106.46f, 106.46f, 107.48f, 108.48f, 108.49f, 109.49f, 110.50f, 110.51f, 111.51f, 112.53f };
     private float hd = 0.0f;
 
     public List<GameObject>  twoCubes;
     public bool secondWave ;
+
+    private float[] floatArray;
+    public string rawData;
+    public float secondWaveTime;
 
     public GameObject message;
     // Start is called before the first frame update
     void Start()
     {
 
+
+        string[] spawns = rawData.Split(',');
+        floatArray = new float[spawns.Length];
+        foreach (string x in spawns)
+        {
+            Debug.Log(x);
+            myStack.Enqueue(float.Parse(x));
+        }
         //audioData = GetComponent<AudioSource>();
         //audioData.Play(0);
         secondWave = false;
-        foreach (float x in floatArray)
-        {
-            myStack.Enqueue(x);
-        }
+     
         hd = (float)myStack.Dequeue();
 
         changeTo("yellowCube", "blueCube");
@@ -62,14 +70,14 @@ public class spawner : MonoBehaviour
         {
             Debug.Log("this is the time: " + timer);
 
-            if (timer > 25.70f && !secondWave)
+            if (timer > secondWaveTime && !secondWave)
             {
                 secondWave = true;
                 changeTo("redCube", "grayCube");
                 message.SetActive(true);
             }
 
-            if (timer > 35.5f)
+            if (timer > secondWaveTime + 7.0f)
             {
                 message.SetActive(false);
 
