a82a38cc4b9d0c1b3a46c929682d3591038d3be3
zzahra2 <zzahra2@uic.edu>
2019-11-30 20:08:45 -0600

    Adds level select to targetrange

---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+
+    // Scene we want to load
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
